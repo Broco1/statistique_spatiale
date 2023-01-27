ef3ff8c3f0f63aef6efba5b8feeb75988feee00c
nloaxc <nolwenn.loaec@eleve.ensai.fr>
2023-01-27 15:38:01 +0000

    Suite TP2

--- tp2_exo1.R
+++ tp2_exo1.R
@@ -1,11 +1,17 @@
+# récupérer avec git clone $GIT_REPOSITORY
+
+
 #st_read du package sf. Vous rajouterez
 #également la commande suivante au sein de la fonction options = "ENCODING=WINDOWS-1252".
 #Quelles informations apparaissent dans la console ?
 library(sf)
 library(dplyr)
 library(units)
+library(ggplot2)
 
 carte_commune<-st_read("fonds/commune_francemetro_2021.shp", options = "ENCODING=WINDOWS-1252")
+# Attention, il faut uploader tous le fichier zip
+
 #' nbre d'éléments
 #' Simple feature collection with 34836 features and 16 fields
 #' Format
@@ -14,13 +20,15 @@ carte_commune<-st_read("fonds/commune_francemetro_2021.shp", options = "ENCODING
 #' Bounding box:  xmin: 99225.97 ymin: 6049647 xmax: 1242375 ymax: 7110480
 #' Référence de la projection
 #' Projected CRS: RGF93 / Lambert-93
+#' 
+#' # pour récupérer un dataframe st_drop
 
 summary(carte_commune)
 
 # Nous trouvons des variable habituelles des datagrame (character, numerical)
 # et une variable geometry contenant des multiplygon
 
-View(carte_commune)
+#View(carte_commune)
 head(carte_commune, 10)$geometry
 str(carte_commune)
 
@@ -58,8 +66,8 @@ str(communes_Bretagne$surf3)
 # surf2 est donné en m^2
 # surf3 est donné en km^2
 
-# Les surfaces ne sont pas les même --> Evolution des frontières des communes d'une année à l'autre/ 
-# prise en compte des lacs ?
+# Les surfaces ne sont pas les même --> manque de précision des fonds de carte 
+# (plus de précision pas nécessaire pour la représentation graphique)
 
 dept_bretagne<-communes_Bretagne %>% 
   group_by(dep) %>% 
@@ -67,19 +75,77 @@ dept_bretagne<-communes_Bretagne %>%
 
 plot(dept_bretagne[,2])
 
-fond_carte<-st_union(summarize(communes_Bretagne%>% 
+fond_carte1<-st_union(summarize(communes_Bretagne%>% 
                                  group_by(dep) %>% 
                                  summarise(superficie=sum(surf3))
 ))
-fond_carte<-st_union(summarize(communes_Bretagne
+fond_carte2<-st_union(summarize(communes_Bretagne
 ))
-plot(fond_carte)
+plot(fond_carte1)
+plot(fond_carte1)
+# Remarque le group_by et la fonction st_union réalisent la même population
 
 # Créer une table “centroid_dept_bretagne” contenant les centroïdes des départements bretons.
 
+?st_centroid
+st_centroid(communes_Bretagne)
+plot(st_centroid(communes_Bretagne %>% 
+                   group_by(dep) %>% 
+                   summarise(superficie=sum(surf3))) %>% 
+       select(superficie))
+centroid_dept_bretagne<-st_centroid(communes_Bretagne %>% 
+                                      group_by(dep) %>% 
+                                      summarise(superficie=sum(surf3)))
+ggplot()+
+  geom_sf(data=centroid_dept_bretagne, fill='steelblue',col='grey45')
+ggplot()+
+  geom_sf(data=communes_Bretagne, fill='steelblue',col='grey45')+
+  theme_void()
 
 
+str(centroid_dept_bretagne)
+# La géométrie est un sfc_POINT ; ce sont des points
+plot(dept_bretagne %>% 
+       select(geometry))
+plot(centroid_dept_bretagne %>% select(geometry), add=T)
 
+ggplot()+
+  geom_sf(data=dept_bretagne,fill='steelblue')+
+  geom_sf(data=centroid_dept_bretagne)+
+  theme_light()
 
+# Ajouter le nom du départment dans le fond de centroïdes. La variable aura pour nom dept_lib.
+# Plusieurs solutions sont possibles, la plus propre étant d’utiliser une petite table de passage et de la
+# fusionner avec le fond de centroïdes.
+transition_dept_nom<-data.frame(dep=c("22","29","35","56"),
+                           dept_lib=c("Côtes d'Armor","Finistère","Ille-et-Villaine","Morbihan"))
 
-  
+centroid_dept_bretagne<-centroid_dept_bretagne %>% 
+  left_join(transition_dept_nom,by=c("dep"="dep"))
+
+ggplot()+
+  geom_sf(data=dept_bretagne,fill='steelblue')+
+  geom_sf(data=centroid_dept_bretagne)+
+  #geom_sf_label(data=centroid_dept_bretagne, aes(label=dept_lib),size=4)+
+  geom_sf_text(data=centroid_dept_bretagne, aes(label=dept_lib),size=2)+
+  theme_light()
+
+st_intersects(centroid_dept_bretagne,communes_Bretagne)
+# Le premier centroid côtes d'armor est dans la commune 148 (ligne) au code Insee 22170
+st_intersection(centroid_dept_bretagne,communes_Bretagne)
+# Même résultat, mais avec des polygones plutôt que des listes
+
+st_distance(centroid_dept_bretagne[centroid_dept_bretagne$dep=="22",],
+            communes_Bretagne[communes_Bretagne$libelle=="Saint-Brieuc",])
+# La distance en le centre des côtes d'armor et son chef lieu est d'environ 9 km
+set_units(st_distance(centroid_dept_bretagne[centroid_dept_bretagne$dep=="22",],
+                      communes_Bretagne[communes_Bretagne$libelle=="Saint-Brieuc",]),
+          value = km)
+
+buffer<-st_buffer(centroid_dept_bretagne,20000)
+plot(buffer$geometry)
+str(buffer)
+
+st_intersection(buffer,
+              communes_Bretagne %>% 
+                filter(libelle%in%c("Saint-Brieuc","Quimper","Rennes","Vannes")))
